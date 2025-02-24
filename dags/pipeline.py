cdc2984903efd34febaa0fcfa63c014a5f44e39e
Austin Ngobeni <kngobeni223@gmail.com>
2025-02-24 12:49:32 +0200

    create dag

---
+++ dags/pipeline.py
@@ -0,0 +1,27 @@
+import json, requests, os
+from airflow import DAG
+from airflow.decorators import task
+from airflow.exceptions import AirflowException
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.dates import days_ago
+import pandas as pd
+from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.models import Variable
+
+logger = LoggingMixin().log
+
+default_args = {
+    "owner":"crime-data",
+    "start_date":days_ago(1)
+}
+
+with DAG(
+    dag_id="crime-data-id",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+
+    @task
+    def load_files():
+        pass
