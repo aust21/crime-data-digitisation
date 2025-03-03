f9a523c62d959f61a7f59c3c87fc3d73f38a87f7
Austin Ngobeni <kngobeni223@gmail.com>
2025-03-03 14:20:16 +0200

    write data from redis to postgres

--- backend/dags/aggregated_data_model.py
+++ backend/dags/aggregated_data_model.py
@@ -14,3 +14,12 @@ class CrimeByCategory(Base):
     id = Column(Integer, primary_key=True)
     category = Column(String(300))
     total_count = Column(Integer)
+
+class CrimeData(Base):
+    __tablename__ = 'crime_data'
+
+    id = Column(Integer, primary_key=True)
+    geography = Column(String(300))
+    crime_category = Column(String(300))
+    financial_year = Column(String(300))
+    crime_count = Column(Integer)
--- backend/dags/pipeline.py
+++ backend/dags/pipeline.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 dag_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(dag_path)
 from pendulum import datetime, duration
-from aggregated_data_model import CrimeByCategory
+from aggregated_data_model import CrimeByCategory, CrimeData
 import redis
 from airflow.hooks.base import BaseHook
 
@@ -30,6 +30,14 @@ default_args = {
     "retry_delay": duration(seconds=2),
     "retry_exponential_backoff": True,
     "max_retry_delay": duration(hours=2),
+}
+
+conf = {
+    'host': Variable.get("MASTER_ENDPOINT", default_var=None),
+    'port': Variable.get("MASTER_PORT", default_var=None),
+    'database': Variable.get("MASTER_DBNAME", default_var=None),
+    'user': Variable.get("MASTER_USERNAME", default_var=None),
+    'password': Variable.get("MASTER_PASSWORD", default_var=None)
 }
 
 with DAG(
@@ -74,6 +82,37 @@ with DAG(
             logger.error(f"Something went wrong with Redis: {str(e)}",
                          exc_info=True)
 
+    @task
+    def redis_to_postgres():
+        conn = BaseHook.get_connection("redis")
+        try:
+            r = redis.Redis(host=conn.host, port=6379, decode_responses=True)
+            engine = create_engine(
+                "postgresql://{user}:{password}@{host}:{port}/{database}".format(
+                    **conf
+                )
+            )
+            session = sessionmaker(bind=engine)
+            session_obj = session()
+            for key in r.keys():
+                row = json.loads(r.get(key))
+                geo = row.get("Geography")
+                if not geo:
+                    continue
+                data = CrimeData(
+                    geography=geo,
+                    crime_category = row["Crime Category"],
+                    financial_year = row["Financial Year"],
+                    crime_count = row["Count"]
+                )
+                session_obj.add(data)
+            session_obj.commit()
+            logger.info("Data written to postgres from redis successfully")
+        except Exception as e:
+            logger.error(
+                f"Something went wrong with Redis to Postgres: {str(e)}",
+                exc_info=True
+                )
 
     @task
     def transform(data_dir, file_name):
@@ -85,13 +124,7 @@ with DAG(
 
     @task
     def load(data):
-        conf = {
-            'host': Variable.get("MASTER_ENDPOINT", default_var=None),
-            'port': Variable.get("MASTER_PORT", default_var=None),
-            'database': Variable.get("MASTER_DBNAME", default_var=None),
-            'user': Variable.get("MASTER_USERNAME", default_var=None),
-            'password': Variable.get("MASTER_PASSWORD", default_var=None)
-        }
+
         logger.info(f"port: {conf['port']}, password: {conf['password']}, "
                     f"database: {conf['database']}, user: {conf['user']}, "
                     f"host: {conf['host']}")
@@ -128,6 +161,7 @@ with DAG(
         file_name, data_dir)
 
     load_to_redis(os.path.join(data_dir, file_name))
+    redis_to_postgres()
 
     data = transform(data_dir, file_name)
     load(data)
